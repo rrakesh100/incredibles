a26ee460102719d10c868abcbe89a6122ba514c5
Ram <irraju@gmail.com>
2018-07-22 12:44:40 -0500

    Splash Screen

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View , Dimensions} from 'react-native';
-import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
+import { Dimensions, StyleSheet, View, YellowBox } from 'react-native';
+import { SceneMap, TabView } from 'react-native-tab-view';
+YellowBox.ignoreWarnings([
+  'Encountered an error loading page', // WebView uri: result.url and url failing to load - "bloomberg suneq" https://github.com/facebook/react-native/issues/7839#issuecomment-224111608
+  'Deprecation warning: moment construction falls back to js Date. This is discouraged and will be removed in upcoming major release. Please refer to http://momentjs.com/guides/#/warnings/js-date/ for more info.',
+  'Task orphaned for request ',
+  'Remote debugger is in a background tab which may cause apps to perform slowly',
+  ]);
+  console.disableYellowBox = true;
 
 export default class App extends React.Component {
   
