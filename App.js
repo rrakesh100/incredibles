595a92d22127db157aefcb671b7d9dc028b7bc5a
Rakesh <rrampall@cisco.com>
2018-07-22 21:01:56 +0530

    Tabs in main page

--- App.js
+++ App.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Dimensions} from 'react-native';
+import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 
 export default class App extends React.Component {
+  
+  state = {
+    index: 0,
+    routes: [
+      { key: 'home', title: 'Home' },
+      { key: 'academicStudies', title: 'Academic Studies' },
+      { key: 'competitiveExams', title: 'Competitive Exams' }
+    ],
+  };
+  
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <TabView
+         navigationState={this.state}
+         renderScene={SceneMap({
+           home: FirstRoute,
+           academicStudies: SecondRoute,
+           competitiveExams : ThirdRoute
+         })}
+         onIndexChange={index => this.setState({ index })}
+         initialLayout={{ width: Dimensions.get('window').width }}
+       />
     );
   }
 }
+
+const FirstRoute = () => (
+  <View style={[styles.container, { backgroundColor: '#ff4081' }]} />
+);
+const SecondRoute = () => (
+  <View style={[styles.container, { backgroundColor: '#673ab7' }]} />
+);
+
+const ThirdRoute = () => (
+  <View style={[styles.container, { backgroundColor: '#303ab7' }]} />
+);
 
 const styles = StyleSheet.create({
   container: {
