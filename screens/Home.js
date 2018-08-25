15d2b7f8974cddefdc270a08990eab14a8a1f26c
Rakesh <rrampall@cisco.com>
2018-08-25 09:19:32 +0530

    Drawer navigator changes from Ram

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { Dimensions, StyleSheet, View, YellowBox } from 'react-native';
-<<<<<<< HEAD
 import { SceneMap, TabView, TabBar } from 'react-native-tab-view';
 import Competitive from './Compititive';
-import HomePage from './HomePage';
 import AcademicStudies from './AcademicStudies';
 import Second from "./../components/second";
 import OnlineTests from "./../components/OnlineTests";
 
-import { SceneMap, TabBar, TabView } from 'react-native-tab-view';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Ionicon from 'react-native-vector-icons/Ionicons';
-import Second from "./../components/second";
-import Competitive from './Compititive';
 import Estore from './Estore';
 import { DrawerNavigator } from 'react-navigation';
 import {
